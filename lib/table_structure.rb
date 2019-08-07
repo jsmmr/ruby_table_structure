411c1727d2fe1481757f5c536daadeccffa09794
Jun Shimamura <jsmmr@icloud.com>
2019-08-08 04:57:06 +0900

    Initial commit

---
+++ lib/table_structure.rb
@@ -0,0 +1,6 @@
+require "table_structure/version"
+
+module TableStructure
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/table_structure_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe TableStructure do
+  it "has a version number" do
+    expect(TableStructure::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
