08f92e0ce8ef48b2cd2860d362b3351a6b9a40d4
Jun Shimamura <jsmmr@icloud.com>
2019-08-28 14:19:12 +0900

    Refactor spec

--- spec/table_structure/schema/definition_spec.rb
+++ spec/table_structure/schema/definition_spec.rb
@@ -3,262 +3,161 @@
 RSpec.describe TableStructure::Schema::Definition do
   let(:options) { {} }
 
-  context 'pattern 1' do
-    let(:definitions) do
-      [
-        {}
-      ]
+  describe '#compile' do
+    context 'pattern 1' do
+      let(:definitions) do
+        [
+          {}
+        ]
+      end
+
+      subject { described_class.new(definitions, options).compile }
+
+      it 'compiles definitions' do
+        expect(subject.size).to eq 1
+        expect(subject[0][:name]).to be_nil
+        expect(subject[0][:key]).to be_nil
+        expect(subject[0][:value]).to be_nil
+        expect(subject[0][:size]).to eq 1
+      end
     end
 
-    let(:compiled_definitions) { described_class.new(definitions, options).compile }
+    context 'pattern 2' do
+      let(:definitions) do
+        [
+          {
+            name: 'Name',
+            key: :name,
+            value: 'Taro',
+            size: 1
+          }
+        ]
+      end
 
-    describe 'compiled definitions size' do
-      subject { compiled_definitions.size }
-      it { is_expected.to eq 1 }
+      subject { described_class.new(definitions, options).compile }
+
+      it 'compiles definitions' do
+        expect(subject.size).to eq 1
+        expect(subject[0][:name]).to eq 'Name'
+        expect(subject[0][:key]).to eq :name
+        expect(subject[0][:value]).to eq 'Taro'
+        expect(subject[0][:size]).to eq 1
+      end
     end
 
-    describe '#name' do
-      subject { compiled_definitions[0][:name] }
-      it { is_expected.to be_nil }
+    context 'pattern 3' do
+      let(:definitions) do
+        [
+          {
+            name: ['Pet 1', 'Pet 2', 'Pet 3']
+          }
+        ]
+      end
+
+      subject { described_class.new(definitions, options).compile }
+
+      it 'compiles definitions' do
+        expect(subject.size).to eq 1
+        expect(subject[0][:name]).to eq ['Pet 1', 'Pet 2', 'Pet 3']
+        expect(subject[0][:key]).to be_nil
+        expect(subject[0][:value]).to be_nil
+        expect(subject[0][:size]).to eq 3
+      end
     end
 
-    describe '#key' do
-      subject { compiled_definitions[0][:key] }
-      it { is_expected.to be_nil }
+    context 'pattern 4' do
+      let(:definitions) do
+        [
+          {
+            key: %i[pet1 pet2 pet3]
+          }
+        ]
+      end
+
+      subject { described_class.new(definitions, options).compile }
+
+      it 'compiles definitions' do
+        expect(subject.size).to eq 1
+        expect(subject[0][:name]).to be_nil
+        expect(subject[0][:key]).to eq %i[pet1 pet2 pet3]
+        expect(subject[0][:value]).to be_nil
+        expect(subject[0][:size]).to eq 3
+      end
     end
 
-    describe '#value' do
-      subject { compiled_definitions[0][:value] }
-      it { is_expected.to be_nil }
+    context 'pattern 5' do
+      let(:definitions) do
+        [
+          {
+            name: ['Pet 1', 'Pet 2', 'Pet 3'],
+            key: %i[pet1 pet2]
+          }
+        ]
+      end
+
+      subject { described_class.new(definitions, options).compile }
+
+      it 'compiles definitions' do
+        expect(subject.size).to eq 1
+        expect(subject[0][:name]).to eq ['Pet 1', 'Pet 2', 'Pet 3']
+        expect(subject[0][:key]).to eq %i[pet1 pet2]
+        expect(subject[0][:value]).to be_nil
+        expect(subject[0][:size]).to eq 3
+      end
     end
 
-    describe '#size' do
-      subject { compiled_definitions[0][:size] }
-      it { is_expected.to eq 1 }
-    end
-  end
+    context 'pattern 6' do
+      let(:definitions) do
+        [
+          {
+            name: ['Pet 1', 'Pet 2', 'Pet 3'],
+            key: %i[pet1 pet2 pet3]
+          }
+        ]
+      end
 
-  context 'pattern 2' do
-    let(:definitions) do
-      [
-        {
-          name: 'Name',
-          key: :name,
-          value: 'Taro',
-          size: 1
-        }
-      ]
-    end
+      subject { described_class.new(definitions, options).compile }
 
-    let(:compiled_definitions) { described_class.new(definitions, options).compile }
-
-    describe 'compiled definitions size' do
-      subject { compiled_definitions.size }
-      it { is_expected.to eq 1 }
+      it 'compiles definitions' do
+        expect(subject.size).to eq 1
+        expect(subject[0][:name]).to eq ['Pet 1', 'Pet 2', 'Pet 3']
+        expect(subject[0][:key]).to eq %i[pet1 pet2 pet3]
+        expect(subject[0][:value]).to be_nil
+        expect(subject[0][:size]).to eq 3
+      end
     end
 
-    describe '#name' do
-      subject { compiled_definitions[0][:name] }
-      it { is_expected.to eq 'Name' }
-    end
-
-    describe '#key' do
-      subject { compiled_definitions[0][:key] }
-      it { is_expected.to eq :name }
-    end
-
-    describe '#value' do
-      subject { compiled_definitions[0][:value] }
-      it { is_expected.to eq 'Taro' }
-    end
-
-    describe '#size' do
-      subject { compiled_definitions[0][:size] }
-      it { is_expected.to eq 1 }
-    end
-  end
-
-  context 'pattern 3' do
-    let(:definitions) do
-      [
-        {
-          name: ['Pet 1', 'Pet 2', 'Pet 3']
-        }
-      ]
-    end
-
-    let(:compiled_definitions) { described_class.new(definitions, options).compile }
-
-    describe 'compiled definitions size' do
-      subject { compiled_definitions.size }
-      it { is_expected.to eq 1 }
-    end
-
-    describe '#name' do
-      subject { compiled_definitions[0][:name] }
-      it { is_expected.to eq ['Pet 1', 'Pet 2', 'Pet 3'] }
-    end
-
-    describe '#key' do
-      subject { compiled_definitions[0][:key] }
-      it { is_expected.to be_nil }
-    end
-
-    describe '#value' do
-      subject { compiled_definitions[0][:value] }
-      it { is_expected.to be_nil }
-    end
-
-    describe '#size' do
-      subject { compiled_definitions[0][:size] }
-      it { is_expected.to eq 3 }
-    end
-  end
-
-  context 'pattern 4' do
-    let(:definitions) do
-      [
-        {
-          key: %i[pet1 pet2 pet3]
-        }
-      ]
-    end
-
-    let(:compiled_definitions) { described_class.new(definitions, options).compile }
-
-    describe 'compiled definitions size' do
-      subject { compiled_definitions.size }
-      it { is_expected.to eq 1 }
-    end
-
-    describe '#name' do
-      subject { compiled_definitions[0][:name] }
-      it { is_expected.to be_nil }
-    end
-
-    describe '#key' do
-      subject { compiled_definitions[0][:key] }
-      it { is_expected.to eq %i[pet1 pet2 pet3] }
-    end
-
-    describe '#value' do
-      subject { compiled_definitions[0][:value] }
-      it { is_expected.to be_nil }
-    end
-
-    describe '#size' do
-      subject { compiled_definitions[0][:size] }
-      it { is_expected.to eq 3 }
-    end
-  end
-
-  context 'pattern 5' do
-    let(:definitions) do
-      [
-        {
-          name: ['Pet 1', 'Pet 2', 'Pet 3'],
-          key: %i[pet1 pet2]
-        }
-      ]
-    end
-
-    let(:compiled_definitions) { described_class.new(definitions, options).compile }
-
-    describe 'compiled definitions size' do
-      subject { compiled_definitions.size }
-      it { is_expected.to eq 1 }
-    end
-
-    describe '#name' do
-      subject { compiled_definitions[0][:name] }
-      it { is_expected.to eq ['Pet 1', 'Pet 2', 'Pet 3'] }
-    end
-
-    describe '#key' do
-      subject { compiled_definitions[0][:key] }
-      it { is_expected.to eq %i[pet1 pet2] }
-    end
-
-    describe '#value' do
-      subject { compiled_definitions[0][:value] }
-      it { is_expected.to be_nil }
-    end
-
-    describe '#size' do
-      subject { compiled_definitions[0][:size] }
-      it { is_expected.to eq 3 }
-    end
-  end
-
-  context 'pattern 6' do
-    let(:definitions) do
-      [
-        {
-          name: ['Pet 1', 'Pet 2', 'Pet 3'],
-          key: %i[pet1 pet2 pet3]
-        }
-      ]
-    end
-
-    let(:compiled_definitions) { described_class.new(definitions, options).compile }
-
-    describe 'compiled definitions size' do
-      subject { compiled_definitions.size }
-      it { is_expected.to eq 1 }
-    end
-
-    describe '#name' do
-      subject { compiled_definitions[0][:name] }
-      it { is_expected.to eq ['Pet 1', 'Pet 2', 'Pet 3'] }
-    end
-
-    describe '#key' do
-      subject { compiled_definitions[0][:key] }
-      it { is_expected.to eq %i[pet1 pet2 pet3] }
-    end
-
-    describe '#value' do
-      subject { compiled_definitions[0][:value] }
-      it { is_expected.to be_nil }
-    end
-
-    describe '#size' do
-      subject { compiled_definitions[0][:size] }
-      it { is_expected.to eq 3 }
-    end
-  end
-
-  context 'pattern 7' do
-    let(:definitions) do
-      [
-        {
-          name: 'ID',
-          value: 1
-        },
-        {
-          name: 'Name',
-          value: 'Taro'
-        },
-        {
-          name: ['Pet 1', 'Pet 2', 'Pet 3'],
-          value: %w[cat dog]
-        },
-        lambda do |*|
-          %w[Q1 Q2 Q3].map do |question_id|
-            {
-              name: question_id,
-              value: 'yes'
-            }
+    context 'pattern 7' do
+      let(:definitions) do
+        [
+          {
+            name: 'ID',
+            value: 1
+          },
+          {
+            name: 'Name',
+            value: 'Taro'
+          },
+          {
+            name: ['Pet 1', 'Pet 2', 'Pet 3'],
+            value: %w[cat dog]
+          },
+          lambda do |*|
+            %w[Q1 Q2 Q3].map do |question_id|
+              {
+                name: question_id,
+                value: 'yes'
+              }
+            end
           end
-        end
-      ]
-    end
+        ]
+      end
 
-    let(:compiled_definitions) { described_class.new(definitions, options).compile }
+      subject { described_class.new(definitions, options).compile }
 
-    describe 'compiled definitions size' do
-      subject { compiled_definitions.size }
-      it { is_expected.to eq 6 }
+      it 'compiles definitions' do
+        expect(subject.size).to eq 6
+      end
     end
   end
 end
