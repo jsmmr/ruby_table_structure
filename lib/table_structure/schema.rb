33f66c959688e998031006d65702754ed2f8fc33
Jun Shimamura <jsmmr@icloud.com>
2019-12-15 14:22:08 +0900

    Add deprecation message for some optional parameters

--- lib/table_structure/schema.rb
+++ lib/table_structure/schema.rb
@@ -10,20 +10,35 @@ module TableStructure
       klass.extend(DSL::ResultBuilder)
     end
 
-    DEFAULT_OPTIONS = {
+    def initialize(
+      name: self.class.name,
+      context: nil,
       name_prefix: nil,
       name_suffix: nil,
       key_prefix: nil,
       key_suffix: nil,
-      nil_definitions_ignored: false
-    }.freeze
+      nil_definitions_ignored: false,
+      **deprecated_options
+    )
+      unless deprecated_options.empty?
+        caller_location = caller_locations(1, 1)
+        deprecated_options.keys.each do |k|
+          warn "[TableStructure] Specify #{k} option on the writer or the iterator. #{caller_location}"
+        end
+      end
 
-    def initialize(context: nil, name: self.class.name, **options)
       column_definitions = [].concat(self.class.column_definitions)
       context_builders = {}.merge!(self.class.context_builders)
       column_converters = {}.merge!(self.class.column_converters)
       result_builders = {}.merge!(self.class.result_builders)
-      options = DEFAULT_OPTIONS.merge(self.class.options).merge(options)
+      options = {
+        name_prefix: name_prefix,
+        name_suffix: name_suffix,
+        key_prefix: key_prefix,
+        key_suffix: key_suffix,
+        nil_definitions_ignored: nil_definitions_ignored
+      }.merge!(self.class.options).merge!(deprecated_options)
+
       @table_structure_schema_definition_ =
         Definition.new(
           name,
