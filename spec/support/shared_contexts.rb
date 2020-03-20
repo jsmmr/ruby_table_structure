eefbb110eaec4a5f4704a5129080f3279df76cbd
Jun Shimamura <jsmmr@icloud.com>
2020-03-20 13:24:42 +0900

    Add header step option

--- spec/support/shared_contexts.rb
+++ spec/support/shared_contexts.rb
@@ -42,3 +42,111 @@ RSpec.shared_context 'users' do
     ]
   end
 end
+
+RSpec.shared_context 'table_structured_array' do
+  let(:header_row) do
+    [
+      'ID',
+      'Name',
+      'Pet 1',
+      'Pet 2',
+      'Pet 3',
+      'Q1',
+      'Q2',
+      'Q3'
+    ]
+  end
+
+  let(:body_row_taro) do
+    [
+      1,
+      '太郎',
+      'cat',
+      'dog',
+      nil,
+      'yes',
+      'no',
+      'yes'
+    ]
+  end
+
+  let(:body_row_hanako) do
+    [
+      2,
+      '花子',
+      'rabbit',
+      'turtle',
+      'squirrel',
+      'yes',
+      'yes',
+      'no'
+    ]
+  end
+
+  let(:body_row_jiro) do
+    [
+      3,
+      '次郎',
+      'tiger',
+      'elephant',
+      'doragon',
+      'no',
+      'yes',
+      nil
+    ]
+  end
+end
+
+RSpec.shared_context 'table_structured_hash' do
+  let(:header_row) do
+    {
+      id: 'ID',
+      name: 'Name',
+      pet1: 'Pet 1',
+      pet2: 'Pet 2',
+      pet3: 'Pet 3',
+      q1: 'Q1',
+      q2: 'Q2',
+      q3: 'Q3'
+    }
+  end
+
+  let(:body_row_taro) do
+    {
+      id: 1,
+      name: '太郎',
+      pet1: 'cat',
+      pet2: 'dog',
+      pet3: nil,
+      q1: 'yes',
+      q2: 'no',
+      q3: 'yes'
+    }
+  end
+
+  let(:body_row_hanako) do
+    {
+      id: 2,
+      name: '花子',
+      pet1: 'rabbit',
+      pet2: 'turtle',
+      pet3: 'squirrel',
+      q1: 'yes',
+      q2: 'yes',
+      q3: 'no'
+    }
+  end
+
+  let(:body_row_jiro) do
+    {
+      id: 3,
+      name: '次郎',
+      pet1: 'tiger',
+      pet2: 'elephant',
+      pet3: 'doragon',
+      q1: 'no',
+      q2: 'yes',
+      q3: nil
+    }
+  end
+end
