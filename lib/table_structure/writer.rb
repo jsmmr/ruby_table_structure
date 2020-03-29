eca07f06aec9371e5b4d85fe367c639a469af2ca
Jun Shimamura <jsmmr@icloud.com>
2020-03-30 03:08:10 +0900

    Remove deprecated options from write method

--- lib/table_structure/writer.rb
+++ lib/table_structure/writer.rb
@@ -38,43 +38,16 @@ module TableStructure
       items,
       to:,
       method: @options[:method],
-      **deprecated_options,
       &block
     )
-      header = @options[:header]
-      row_type = @options[:row_type]
-
-      if deprecated_options.key?(:header)
-        header = deprecated_options[:header]
-        warn '[TableStructure] Use :header option on initialize method.'
-      end
-
-      if deprecated_options.key?(:header_omitted)
-        header_omitted = deprecated_options[:header_omitted]
-        warn "[TableStructure] `header_omitted: #{!!header_omitted}` option has been deprecated. Use `header: #{!header_omitted}` option instead."
-        header = !header_omitted
-      end
-
-      if deprecated_options.key?(:header_context)
-        header_context = deprecated_options[:header_context]
-        warn '[TableStructure] `:header_context` option has been deprecated. Use `header: { context: ... }` option instead.'
-        header = { context: header_context }
-      end
-
-      if deprecated_options.key?(:row_type)
-        row_type = deprecated_options[:row_type]
-        warn '[TableStructure] Use :row_type option on initialize method.'
-      end
-
-      if deprecated_options.key?(:result_type)
-        warn '[TableStructure] `:result_type` option has been deprecated. Use `:row_type` option instead.'
-        row_type = deprecated_options[:result_type]
-      end
-
       output = Output.new(to, method: method)
 
       Iterator
-        .new(@schema, header: header, row_type: row_type)
+        .new(
+          @schema,
+          header: @options[:header],
+          row_type: @options[:row_type]
+        )
         .iterate(items, &block)
         .each { |row| output.write(row) }
 
